351f4e452b610e3490ca16b9facd516ffcdd7650
Денис <permyakov.d.a.92@mail.ru>
2026-09-22 12:36:17 +0500

    подготовка первичных файлов css и js

---
+++ scripts/no-js.js
@@ -0,0 +1,11 @@
+// Елси js не сработал на сайте то нужно показать соответствующее уведомление.
+// если js сработал, то просто убираем класс no-js с body
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Ссылка на body с классом .no-js
+    const bodyElem = document.querySelector('.no-js');
+
+    // Отключить класс .no-js т.к. js подключен
+    bodyElem.classList.remove('no-js');
+});
